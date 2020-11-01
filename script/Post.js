7234a229e795ef980e13634ca99b3f15eee765f2
Jhon Garcia <metalprime57@gmail.com>
2020-11-01 00:28:24 -0500

    update js finished

--- script/Post.js
+++ script/Post.js
@@ -10,15 +10,43 @@ class Post{
          component.innerHTML = `
          <div class="publication__description">
             <p class="publication__text">${this.user.post}</p>
-            <p class="publication__user">${this.user.username}</p> 
+            <p class="publication__user">@${this.user.user}</p> 
          </div>
          <div class="publication__comment">
          </div>
          <div class="publication__opinion">
-         <input type="text" placeholder="Escribe tu nombre de usuario" name="textcomment" class="publication__input">
+         <input type="text" placeholder="Escribe tu Comentario" name="textcomment" class="publication__input">
          <button class="publication__btn">Responder</button> 
          </div>
          `;
 
+        
+
+         
+
+         return component;
+
+        
     }
+
+    comment = () =>{
+        let comment = document.querySelector('.publication__input');
+
+                    console.log("fegvr");
+                        let btnComment = document.querySelector('.publication__btn');
+                        btnComment.addEventListener('click', ()=>{
+                            console.log("FDSFGVZDSBDV")
+                            let Comment = {
+                                comment: comment.value
+                            }
+                            console.log(Comment.comment)
+                            database.ref('post/'+this.user.id+'/comment').push().set(Comment);
+                            
+
+                        });
+                    
+                    
+    }
+
+
 }
--- script/index.js
+++ script/index.js
@@ -1,21 +1,103 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAJAb5xt0drUeCa2dBKMhHf3z8QMaHI2y4",
+    authDomain: "s10project.firebaseapp.com",
+    databaseURL: "https://s10project.firebaseio.com",
+    projectId: "s10project",
+    storageBucket: "s10project.appspot.com",
+    messagingSenderId: "830069995055",
+    appId: "1:830069995055:web:fd00d1377723bb4adbf8da",
+    measurementId: "G-EHCJM6SK1J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 const inputUser = document.querySelector('.inputUser');
 const inputPost = document.querySelector('.inputPost');
 const btnPublicar = document.querySelector('.btnPublicar');
+const post__list = document.querySelector('.post__list')
 const publicationComment = document.querySelector('.publication__comment');
 const database = firebase.database();
 
-btnPublicar.addEventListener('click',function (){
+console.log("wsdffr");
+
+
+btnPublicar.addEventListener('click',function (e){
+    e.preventDefault();
+    console.log("puto");
     if(inputUser.value === '' || inputPost.value === ''){
         alert ("Campo Vacio");
-        return;
+    }else{
+
+        let referencia = database.ref('post').push();
+        let postUser = {
+           id : referencia.key,
+           post : inputPost.value,
+           user: inputUser.value
+        }
+        console.log(postUser);
+        referencia.set(postUser,function(error) {
+            if (error) {
+              // The write failed...
+            } else {
+              // Data saved successfully!
+              console.log("funciono");
+            }
+          });
     }
 
-    let referencia = database.ref('post').push();
-    let postUser = {
-       id : referencia.key,
-       post : inputPost.value,
-       user: inputUser.value,
-    }
-    console.log(postUser);
-    referencia.set(postUser);
 });
+
+
+
+
+    database.ref('post').on('value', function(data) {
+        post__list.innerHTML = '';
+
+        data.forEach(
+            postUser => {
+                let valor = postUser.val();
+                let element =new Post(valor);
+                post__list.appendChild(element.render());
+                element.comment();
+                console.log('post/'+valor.id+'/comment');
+                database.ref('post/'+valor.id+'/comment').on('value', function(i) {
+                    i.forEach(
+                        commentThing => {
+                            let val = commentThing.val();
+                            let listComment = document.querySelector('.publication__comment');
+                            let itemComment = document.createElement('div');
+
+                            itemComment.classList.add("publication__item");
+                            itemComment.innerHTML = val.comment;
+                            listComment.appendChild(itemComment);
+                        }
+                    );
+                    
+                });
+
+            }
+        );
+
+        
+        
+    
+    });
+
+
+    database.ref('post').on('value', function(data) {
+
+        data.forEach(
+            post =>{
+                
+            }
+        )
+    });
+
+
+
+    
+
+
+
